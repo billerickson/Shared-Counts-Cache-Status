9f4141a46577187491cac2dd12dc0e7a063731d5
Bill Erickson <bill.erickson@gmail.com>
2018-05-23 11:49:35 -0500

    rename plugin

--- shared-counts-cache-status.php
+++ shared-counts-cache-status.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Plugin Name: Shared Counts - Prime Cache
- * Plugin URI:  https://github.com/billerickson/Shared-Counts-Prime-Cache
+ * Plugin Name: Shared Counts - Cache Status
+ * Plugin URI:  https://github.com/billerickson/Shared-Counts-Cache-Status
  * Description: Build and check the status of the Shared Counts cache
  * Author:      Bill Erickson
  * Version:     1.0.0
  *
- * Shared Counts - Prime Cache is free software: you can redistribute it and/or modify
+ * Shared Counts - Cache Status is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * any later version.
  *
- * Shared Counts - Prime Cache is distributed in the hope that it will be useful,
+ * Shared Counts - Cache Status is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Shared Counts. If not, see <http://www.gnu.org/licenses/>.
  *
- * @package    SharedCountsPrimeCache
+ * @package    SharedCountsCacheStatus
  * @author     Bill Erickson
  * @since      1.0.0
  * @license    GPL-2.0+
@@ -36,11 +36,11 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @since 1.0.0
  */
-function scpc_settings_init() {
+function sccs_settings_init() {
 
-	register_setting( 'shared_counts_prime_cache', 'shared_counts_prime_cache', false );
+	register_setting( 'shared_counts_cache_status', 'shared_counts_cache_status', false );
 }
-add_action( 'admin_init', 'scpc_settings_init' );
+add_action( 'admin_init', 'sccs_settings_init' );
 
 
 /**
@@ -48,18 +48,18 @@ add_action( 'admin_init', 'scpc_settings_init' );
  *
  * @since 1.0.0
  */
-function scpc_settings_add() {
+function sccs_settings_add() {
 
-	add_options_page( __( 'Shared Counts Cache', 'scpc' ), __( 'Shared Counts Cache', 'scpc' ), 'manage_options', 'shared_counts_prime_cache', 'scpc_settings_page' );
+	add_options_page( __( 'Shared Counts Cache', 'sccs' ), __( 'Shared Counts Cache', 'sccs' ), 'manage_options', 'shared_counts_cache_status', 'sccs_settings_page' );
 }
-add_action( 'admin_menu', 'scpc_settings_add' );
+add_action( 'admin_menu', 'sccs_settings_add' );
 
 /**
  * Build the Settings page.
  *
  * @since 1.0.0
  */
-function scpc_settings_page() {
+function sccs_settings_page() {
 
 	if( ! function_exists( 'shared_counts' ) ) {
 		echo '<p>Error: The <a href="https://wordpress.org/plugins/shared-counts/">Shared Counts</a> plugin must be active.</p>';
@@ -77,7 +77,7 @@ function scpc_settings_page() {
 	} else {
 
 		$counts = scps_shared_counts_posts();
-		$total = scpc_available_posts();
+		$total = sccs_available_posts();
 		$percentage = (int) ( ( $counts / $total) * 100 + .5 );
 		echo '<p>' . $counts . ' of ' . $total . ' items have Shared Counts data (' . $percentage . '%).</p>';
 		if( $total > $counts )
@@ -91,29 +91,29 @@ function scpc_settings_page() {
  * Prime Cache
  *
  */
-function scpc_prime_cache() {
+function sccs_prime_cache() {
 
 	if( ! function_exists( 'shared_counts' ) )
 		return;
 
 	$screen = get_current_screen();
-	if( 'settings_page_shared_counts_prime_cache' != $screen->base )
+	if( 'settings_page_shared_counts_cache_status' != $screen->base )
 		return;
 
 	if( empty( $_GET['prime_cache'] ) )
 		return;
 
-	shared_counts()->core->prime_the_pump( scpc_available_posts(), apply_filters( 'scpc_interval', 20 ) );
+	shared_counts()->core->prime_the_pump( sccs_available_posts(), apply_filters( 'sccs_interval', 20 ) );
 
 
 }
-add_action( 'admin_head', 'scpc_prime_cache' );
+add_action( 'admin_head', 'sccs_prime_cache' );
 
 /**
  * Available Posts
  *
  */
-function scpc_available_posts() {
+function sccs_available_posts() {
 
 	if( ! function_exists( 'shared_counts' ) )
 		return;
